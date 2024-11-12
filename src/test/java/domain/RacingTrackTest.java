0251c952c539dfeab79c0b6d9acdf129f1f8c04f
woosungking <abuga991008@gmail.com>
2024-11-12 22:35:12 +0900

    test<.nitRacingTrackToListOfCarAndPositionIsZero> RacingTrack에서 차의 이름과 위치를 가지고 올 DTO 클래스

--- src/test/java/domain/RacingTrackTest.java
+++ src/test/java/domain/RacingTrackTest.java
@@ -2,7 +2,9 @@ package domain;
 
 import com.java.domain.Car;
 import com.java.domain.racing.RacingTrack;
+import com.java.dto.CarPositionDto;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -22,5 +24,24 @@ public class RacingTrackTest {
         Integer expected = 3;
         //then
         Assertions.assertEquals(expected, racingTrack.size());
+    }
+
+    @Test
+    @DisplayName("특정 자동차를 이동 시켰을때 위치 변경이 잘 이루어 지는가")
+    public void moveCarToCarNameUsing(){
+        //given
+        List<Car> cars = new ArrayList<>();
+        Car s63Amg = Car.of("s63Amg");
+        Car M3 = Car.of("M3");
+        Car CLS400 = Car.of("CLS400");
+        cars.add(s63Amg);
+        cars.add(M3);
+        cars.add(CLS400);
+        racingTrack = RacingTrack.of(cars);
+        //when
+        racingTrack.moveCar(s63Amg);
+        CarPositionDto carPositionDto = racingTrack.getCarPositionDTO(s63Amg);
+        //then
+        Assertions.assertEquals(carPositionDto.getCarPosition(), 1);
     }
 }
