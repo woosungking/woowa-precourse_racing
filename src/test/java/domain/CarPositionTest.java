6a2bce54693ea1255e3d4afb665833bf5644d558
woosungking <abuga991008@gmail.com>
2024-11-12 21:29:20 +0900

    test<.shouldInitializePositionToZero> CarPosition 초기 상태 테스트

--- src/test/java/domain/CarPositionTest.java
+++ src/test/java/domain/CarPositionTest.java
@@ -1,45 +1,29 @@
 package domain;
 
-import com.java.domain.Car;
-import com.java.domain.CarList;
+import com.java.domain.position.CarPosition;
+import com.java.dto.CarPositionDto;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class CarPositionTest {
-
-    private CarList carList;
+    private CarPosition carPosition;
     @BeforeEach
     public void beforeEach(){
-        carList = CarList.of(new ArrayList<>());
+        carPosition = CarPosition.of();
     }
-
     @Test
-    @DisplayName("beforeEach 테스트")
-    public void initCarListTest(){
-        //given
+    @DisplayName("CarPosition 초기화 시 위치는 0으로 설정되어야 한다")
+    public void shouldInitializePositionToZero() {
+        // given: CarPosition 객체가 초기 상태(0)으로 생성됨
+        carPosition = CarPosition.of();
         Integer expected = 0;
-        //when
-        //then
-        Assertions.assertEquals(expected, carList.size());
-    }
 
-    @Test
-    @DisplayName("CarList 생성 테스트")
-    public void carCreateTest(){
-        //given
-        List<Car> cars = new ArrayList<>();
-        cars.add(Car.of("K3"));
-        cars.add(Car.of("K5"));
-        cars.add(Car.of("K7"));
-        cars.add(Car.of("K9"));
-        //when
-        carList.addAllCar(cars);
-        //then
-        Assertions.assertEquals(cars.size(), carList.size());
+        // when: 객체를 DTO로 변환하여 초기 위치 확인
+        CarPositionDto carPositionDto = carPosition.carPositionDto();
+
+        // then: position은 0이어야 함
+        Assertions.assertEquals(expected, carPositionDto.getPosition(), "초기 위치는 0이어야 합니다.");
     }
 }
