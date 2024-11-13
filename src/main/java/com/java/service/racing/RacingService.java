b402b3ad8fa322ddf93870b35d6a86e98dfcd007
woosungking <abuga991008@gmail.com>
2024-11-13 23:00:21 +0900

    feat<.racingInitializer> RacingManager를 생성해주는 매서드

--- src/main/java/com/java/service/racing/RacingService.java
+++ src/main/java/com/java/service/racing/RacingService.java
@@ -1,4 +1,12 @@
 package com.java.service.racing;
 
+import com.java.domain.RacingManager;
+import com.java.domain.car.Car;
+import com.java.domain.view.Retries;
+
+import java.util.List;
+
 public interface RacingService {
+
+    public RacingManager racingInitializer(List<Car> cars, Retries retries);
 }
--- src/main/java/com/java/service/racing/RacingServiceImpl.java
+++ src/main/java/com/java/service/racing/RacingServiceImpl.java
@@ -1,4 +1,20 @@
 package com.java.service.racing;
 
-public class RacingServiceImpl {
+import camp.nextstep.edu.missionutils.Randoms;
+import com.java.domain.RacingManager;
+import com.java.domain.car.Car;
+import com.java.domain.car.CarList;
+import com.java.domain.racing.RacingTrack;
+import com.java.domain.view.Retries;
+
+import java.util.List;
+
+public class RacingServiceImpl implements RacingService{
+    @Override
+    public RacingManager racingInitializer(List<Car> cars, Retries retries) {
+        RacingTrack racingTrack = RacingTrack.of(cars); // 레이싱 트렉에 자동차랑, 위치선정,
+        return RacingManager.of(racingTrack, retries);
+    }
+
+
 }
