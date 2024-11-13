bab802e3a56352ae331044718fbcb50140f00572
woosungking <abuga991008@gmail.com>
2024-11-13 20:33:43 +0900

    feat<.getAllCarInCarList> 리스트를 반환하는 메서드(getter 아님)

--- src/main/java/com/java/service/car/CarService.java
+++ src/main/java/com/java/service/car/CarService.java
@@ -1,10 +1,12 @@
 package com.java.service.car;
 
 import com.java.domain.car.Car;
+import com.java.domain.car.CarList;
 
 import java.util.List;
 
 public interface CarService {
     public List<Car> splitCarNamesToCarList(String carNames);
-    public void addCarToCarList(List<Car> carList);
+    public void addCarToCarList(List<Car> cars, CarList carList);
+    public List<Car> getAllCarInCarList(CarList carList);
 }
