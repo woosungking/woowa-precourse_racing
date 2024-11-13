166b466a9f60e7b4d3fe1a2e64620bfdf56ee3de
woosungking <abuga991008@gmail.com>
2024-11-13 14:33:01 +0900

    test<.decrease> 라운드가 끝나면 시도횟수는 한번씩 줄어야한다.

--- src/test/java/domain/RacingManagerTest.java
+++ src/test/java/domain/RacingManagerTest.java
@@ -44,5 +44,24 @@ public class RacingManagerTest {
         assertEquals(1, carPositionDto.getCarPosition(), "선택된 자동차는 1번만 이동해야 함.");
         System.out.println("자동차 이름: " + carPositionDto.getCarName() + ", 위치: " + carPositionDto.getCarPosition());
     }
+    @Test
+    @DisplayName("한 라운드가 지날때마다, Retries는 1씩 감소해야 한다.")
+    public void decrease(){
+        // given: 초기 자동차 목록과 시도횟수 입력.
+        List<Car> cars = new ArrayList<>();
+        Car avante = Car.of("avante");
+        cars.add(Car.of("E3004matic"));
+        cars.add(Car.of("328i"));
+        cars.add(avante);
 
+        Retries retries = Retries.of(5);
+        racingTrack = RacingTrack.of(cars);
+        racingManager = RacingManager.of(racingTrack, retries);
+        Integer expexted = 4;
+        // when
+        racingManager.reduceRetries();
+        Integer actual = racingManager.getRetriesDto().getRetries();
+        // then
+        assertEquals(expexted, actual);
+    }
 }
