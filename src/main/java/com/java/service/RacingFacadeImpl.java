888951133907034dcf532f777833a1d7ddd2ce26
woosungking <abuga991008@gmail.com>
2024-11-13 18:14:28 +0900

    feat<RacingFacade> 레이싱이라는 하나의 서비스를 통합 실행시켜주는 Facade 클래스

---
+++ src/main/java/com/java/service/RacingFacadeImpl.java
@@ -0,0 +1,10 @@
+package com.java.service;
+
+import com.java.dto.view.StartRacingDto;
+
+public class RacingFacadeImpl implements RacingFacade{
+    @Override
+    public void startRacing(StartRacingDto startRacingDto) {
+
+    }
+}
