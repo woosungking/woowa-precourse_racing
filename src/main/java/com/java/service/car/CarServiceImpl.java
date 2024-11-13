35f2894b1c7ead70d710ee2da9e1607c4ee865a3
woosungking <abuga991008@gmail.com>
2024-11-13 20:33:37 +0900

    feat<.getAllCarInCarList> 리스트를 반환하는 메서드(getter 아님)

--- src/main/java/com/java/service/car/CarServiceImpl.java
+++ src/main/java/com/java/service/car/CarServiceImpl.java
@@ -1,6 +1,7 @@
 package com.java.service.car;
 
 import com.java.domain.car.Car;
+import com.java.domain.car.CarList;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,7 +13,12 @@ public class CarServiceImpl implements CarService{
     }
 
     @Override
-    public void addCarToCarList(List<Car> carList) {
+    public void addCarToCarList(List<Car> cars, CarList carList) {
+        carList.addAllCar(cars);
+    }
 
+    @Override
+    public List<Car> getAllCarInCarList(CarList carList) { //리스트는 방어적 복사지만, 안에 Car객체는 얕은 복사로 반환됨.
+        return carList.findAllCar();
     }
 }
